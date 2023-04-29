bd875b6790347adf5a800e7c0e7f3d27358ddae0
khadija Elhardi <elhardykhadija@gmail.com>
2023-04-29 08:51:47 +0100

    second

--- src/main/java/com/example/patientsmvc/PatientsMvcApplication.java
+++ src/main/java/com/example/patientsmvc/PatientsMvcApplication.java
@@ -15,18 +15,18 @@ public class PatientsMvcApplication {
     public static void main(String[] args) {
         SpringApplication.run(PatientsMvcApplication.class, args);
     }
-    @Bean
+    //@Bean
     CommandLineRunner commandLineRunner(PatientRepository patientRepository){
         return  args -> {
 
             patientRepository.save(
-                    new Patient(null,"Hassan",new Date(),false,12));
+                    new Patient(null,"Hassan",new Date(),false,112));
             patientRepository.save(
                     new Patient(null,"Mohammed",new Date(),true,321));
             patientRepository.save(
-                    new Patient(null,"Yasmine",new Date(),true,65));
+                    new Patient(null,"Yasmine",new Date(),true,165));
             patientRepository.save(
-                    new Patient(null,"Hanae",new Date(),false,32));
+                    new Patient(null,"Hanae",new Date(),false,132));
             patientRepository.findAll().forEach(p->{
                 System.out.println(p.getNom());
             });
--- src/main/java/com/example/patientsmvc/web/PatientController.java
+++ src/main/java/com/example/patientsmvc/web/PatientController.java
@@ -2,12 +2,15 @@ package com.example.patientsmvc.web;
 
 import com.example.patientsmvc.entities.Patient;
 import com.example.patientsmvc.repositories.PatientRepository;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -46,6 +49,31 @@ public class PatientController {
     public List<Patient> lisPatients() {
         return patientRepository.findAll();
     }
+    @GetMapping("/formPatients")
+    public String formPatients(Model model){
+        model.addAttribute("patient",new Patient());
+        return "formPatients";
+
+    }
+    @PostMapping("/save")
+    public String save(Model model, @Valid Patient patient, BindingResult bindingResult,@RequestParam(defaultValue = "0") int page,
+                       @RequestParam(defaultValue = "") String keyword){
+        if(bindingResult.hasErrors()) return "formPatients";
+        patientRepository.save(patient);
+        return "redirect:/index?page"+page+"&keyword="+keyword;
+    }
+    @GetMapping("/editPatient")
+    public String editPatient(Model model,Long id,String keyword,int page){
+        Patient patient=patientRepository.findById(id).orElse(null);
+        if(patient==null) throw new RuntimeException("Patient introuvable");
+        model.addAttribute("patient",patient);
+        model.addAttribute("page",page);
+        model.addAttribute("keyword",keyword);
+        return "editPatient";
+    }
+
+
+
 
 
 }
